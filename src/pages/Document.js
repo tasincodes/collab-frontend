54b3596f47886fbe7909ba34cee021a8ddf2bfe4
tasincodes <shahriartasin2000@gmail.com>
2024-09-10 12:20:12 +0600

    created doc home and login

---
+++ src/pages/Document.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Document Page</h1>
+      <p>Document functionality will go here</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Login functionality will go here</p>
+    </div>
+  );
+}
+
+export default Login;
